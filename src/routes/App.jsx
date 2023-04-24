d10f2b004c83c28c1355af6c951aebd2683505cb
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-04-24 13:28:08 -0500

    feat deploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home';
 import EquidadSocial from '../pages/EquidadSocial';
 import CienciaTecnologia from '../pages/CienciaTecnologia';
@@ -11,18 +11,18 @@ import NotFound from '../pages/NotFound';
 
 const App = ()  => {
         return (
-            <BrowserRouter basename="/FilantropiaEafit">
+            <HashRouter basename="/FilantropiaEafit">
                     <Routes>
                         <Route  path="/" element={<Home/>} />
-                        <Route  path="##/home" element={<Home/>} />
-                        <Route  path="#/equidadSocial" element={<EquidadSocial/>} />
-                        <Route  path="#/cienciaTecnologia" element={<CienciaTecnologia/>} />
-                        <Route  path="#/transformacionHumana" element={<TransformacionHumana/>} />
-                        <Route  path="#/voluntariado" element={<Voluntariado/>} />
-                        <Route  path="#/donacion" element={<Donacion/>} />
+                        <Route  path="/home" element={<Home/>} />
+                        <Route  path="/equidadSocial" element={<EquidadSocial/>} />
+                        <Route  path="/cienciaTecnologia" element={<CienciaTecnologia/>} />
+                        <Route  path="/transformacionHumana" element={<TransformacionHumana/>} />
+                        <Route  path="/voluntariado" element={<Voluntariado/>} />
+                        <Route  path="/donacion" element={<Donacion/>} />
                         <Route  path="*" element={<NotFound/>}/>
                     </Routes>
-            </BrowserRouter>
+            </HashRouter>
         );
 }
 
