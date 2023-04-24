d703b68650578e7cae784e66b127b17d4f7cd8e8
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-04-24 13:48:11 -0500

    feat

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home';
 import EquidadSocial from '../pages/EquidadSocial';
 import CienciaTecnologia from '../pages/CienciaTecnologia';
@@ -8,21 +8,22 @@ import Voluntariado from '../pages/Voluntariado';
 import Donacion from '../pages/Donacion';
 import NotFound from '../pages/NotFound';
 
-const App = () => {
-return (
-<HashRouter basename="/FilantropiaEafit">
-<Routes>
-<Route path="/" element={<Home/>} />
-<Route path="/home" element={<Home/>} />
-<Route path="/equidadSocial" element={<EquidadSocial/>} />
-<Route path="/cienciaTecnologia" element={<CienciaTecnologia/>} />
-<Route path="/transformacionHumana" element={<TransformacionHumana/>} />
-<Route path="/voluntariado" element={<Voluntariado/>} />
-<Route exact path="/donacion" element={<Donacion/>} />
-<Route path="*" element={<NotFound/>}/>
-</Routes>
-</HashRouter>
-);
+
+const App = ()  => {
+        return (
+            <BrowserRouter basename="/FilantropiaEafit">
+                    <Routes>
+                        <Route  path="/" element={<Home/>} />
+                        <Route  path="/home" element={<Home/>} />
+                        <Route  path="/equidadSocial" element={<EquidadSocial/>} />
+                        <Route  path="/cienciaTecnologia" element={<CienciaTecnologia/>} />
+                        <Route  path="/transformacionHumana" element={<TransformacionHumana/>} />
+                        <Route  path="/voluntariado" element={<Voluntariado/>} />
+                        <Route  exact path="/donacion" element={<Donacion/>} />
+                        <Route  path="*" element={<NotFound/>}/>
+                    </Routes>
+            </BrowserRouter>
+        );
 }
 
 export default App;
